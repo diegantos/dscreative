c0831fa57118f012d37b10c618bf42c5587f9575
Diegantos <diegantos8@gmail.com>
2022-12-20 16:55:24 +0100

    new up

--- src/components/Home/Header/Header.jsx
+++ src/components/Home/Header/Header.jsx
@@ -4,12 +4,19 @@ export const Header = () => {
     return(
         <div className="Header">
             <a href="#" className="Header-logo">
-                <img src="" alt="" className="Header-img" />
+                <img src="" alt="Logo" className="Header-img" />
             </a>
 
             <nav className="Header-nav">
-                
+
+                <ul className="Header-ul">
+                    <li className="Header-li">Videography</li>
+                    <li className="Header-li">Photography</li>
+                    <li className="Header-li">Contact</li>
+                </ul>
+
             </nav>
+            
         </div>
     )
 }
