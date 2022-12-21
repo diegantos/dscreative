36474ac5a76e196236738020d3b8e3047fde0ea9
Diegantos <diegantos8@gmail.com>
2022-12-21 13:36:47 +0100

    assets

--- src/components/Home/Header/Header.jsx
+++ src/components/Home/Header/Header.jsx
@@ -3,20 +3,30 @@ import './Header.scss'
 export const Header = () => {
     return(
         <div className="Header">
-            <a href="#" className="Header-logo">
+            <a href="/public/assets/DSWhite.png" className="Header-logo">
                 <img src="" alt="Logo" className="Header-img" />
             </a>
+
+            <button className="Header-button">
+
+            </button>
 
             <nav className="Header-nav">
 
                 <ul className="Header-ul">
-                    <li className="Header-li">Videography</li>
-                    <li className="Header-li">Photography</li>
-                    <li className="Header-li">Contact</li>
+                    <li className="Header-li">
+                        <a href="#" className="Header-a">photograhpy</a>
+                    </li>
+                    <li className="Header-li">
+                        <a href="#" className="Header-a">video</a>
+                    </li>
+                    <li className="Header-li">
+                        <a href="#" className="Header-a">contact</a>
+                    </li>
                 </ul>
 
             </nav>
-            
+
         </div>
     )
 }
