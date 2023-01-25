f34443862728c2406775a92946f6b4c451251570
Diegantos <diegantos8@gmail.com>
2023-01-25 13:49:08 +0100

    ok

--- src/components/Home/Footer/Footer.jsx
+++ src/components/Home/Footer/Footer.jsx
@@ -6,15 +6,21 @@ export const Footer = () => {
             <div className="Footer-Wrapper Wrapper">
                 <ul className="Footer-ul">
                     <li className="Footer-li">
-                        <a href="#" className="Footer-a">Instagram</a>
+                        <a href="#" className="Footer-a"><img src="https://img.icons8.com/sf-regular/48/null/instagram-new.png"/></a>
                     </li>
                     <li className="Footer-li">
-                        <a href="#" className="Footer-a">Vimeo</a>
+                        <a href="#" className="Footer-a"><img src="https://img.icons8.com/sf-regular/48/null/vimeo.png"/></a>
                     </li>
                     <li className="Footer-li">
-                        <a href="#" className="Footer-a">Youtube</a>
+                        <a href="#" className="Footer-a"><img src="https://img.icons8.com/sf-regular/48/null/youtube-play.png"/></a>
+                    </li>
+                    <li className="Footer-li">
+                        <a href="#" className="Footer-a"><img src="https://img.icons8.com/sf-regular/48/null/linkedin.png"/></a>
                     </li>
                 </ul>
+                <div class="register flex jcenter">
+                    <p>diegantos 2022</p>
+                </div>
             </div>
         </div>
     )
--- src/components/Home/Intro/Intro.jsx
+++ src/components/Home/Intro/Intro.jsx
@@ -3,7 +3,7 @@ import './Intro.scss'
 export const Intro = () => {
     return(
         <div className="Intro">
-            <video className='Intro-video' autoplay muted loop src="/assets/Intro.mp4"></video>
+            <video className="Intro-video" autoplay muted loop src="/assets/Intro.mp4"></video>
             {/* <div class="intro__text">
                 <a href="#" class="btn">
                     <span>play showreel</span>
