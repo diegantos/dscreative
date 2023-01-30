6690ac956de37df92c72866ab01295acd1111322
Diegantos <diegantos8@gmail.com>
2023-01-30 22:13:25 +0100

    new intro update

--- src/components/Home/Intro/Intro.jsx
+++ src/components/Home/Intro/Intro.jsx
@@ -9,6 +9,24 @@ export const Intro = () => {
                     <span>play showreel</span>
                 </a>
             </div> */}
+            <div className="Intro-works">
+                <a href="#" className="Intro-a">
+                    <div className="Intro-element Intro-elementVideo">
+                        <div className="Intro-text">
+                            <p className="Intro-p">creatrive filmmaking</p>
+                            <h2 className="Intro-h2">videography</h2>
+                        </div>
+                    </div>
+                </a>
+                {/* <a href="#" className="Intro-a">
+                    <div className="Intro-element"></div>
+                    <div className="Intro-text">
+                        <p className="Intro-p">from nature to festivals</p>
+                        <h2 className="Intro-h2">photography</h2>
+                    </div>
+                </a> */}
+            </div>
         </div>
+        
     )
 }
