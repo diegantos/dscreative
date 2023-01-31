e0f49874762e0a3a3dd90e7c517a317bab97d2cf
Diegantos <diegantos8@gmail.com>
2023-01-31 21:37:11 +0100

    component

--- src/components/Common/Common.jsx
+++ src/components/Common/Common.jsx
@@ -14,20 +14,16 @@ export const Btn = ({ children, clase, url }) => {
 
 export const BackgroundElement = ({ children, clase }) => {
     return(
-        <div className="Intro-works">
-        <a href="#" className="Intro-a">
-            <div className="Intro-element Intro-elementVideo"></div>
-            <div className="Intro-text">
-                <p className="Intro-p">creatrive filmmaking / brand work</p>
-                <h2 className="Intro-h2">videography</h2>
-            </div>
-        </a>
-    </div>
+        <div className="Intro-element Intro-elementVideo"></div>
     )
 }
 
 export const TextEffect = ({ clase }) => {
     return(
-        <div className={`Intro-element ${ clase }`}></div>
+        // <div className={`Intro-element ${ clase }`}></div>
+        <div className="Intro-text">
+            <p className="Intro-p">from nature to festivals</p>
+            <h2 className="Intro-h2">photography</h2>
+        </div>
     )
 }
