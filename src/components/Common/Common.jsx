b046d5a75213308a70c7812d7f0e1163ef91046d
Diegantos <diegantos8@gmail.com>
2023-01-31 20:28:43 +0100

    common

--- src/components/Common/Common.jsx
+++ src/components/Common/Common.jsx
@@ -1,1 +1,23 @@
 import './Common.scss'
+
+export const Btn = ({ children, clase }) => {
+    return(
+        <button 
+            className={`Btn ${ clase }`}
+            title='Play Showreel'>{ children }</button>
+    )
+}
+
+export const Type = ({ children, clase }) => {
+    return(
+        <div className="Intro-works">
+        <a href="#" className="Intro-a">
+            <div className="Intro-element Intro-elementVideo"></div>
+            <div className="Intro-text">
+                <p className="Intro-p">creatrive filmmaking / brand work</p>
+                <h2 className="Intro-h2">videography</h2>
+            </div>
+        </a>
+    </div>
+    )
+}
