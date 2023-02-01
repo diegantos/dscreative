25f1ce32db4f9a04a517a899e18b5f15191db469
Diegantos <diegantos8@gmail.com>
2023-02-01 21:18:01 +0100

    about primary

--- src/components/Contact/About/About.jsx
+++ src/components/Contact/About/About.jsx
@@ -2,8 +2,15 @@ import './About.scss';
 
 export const About = () => {
     return(
-        <div className="About">
-            
+        <div className="About Wrapper">
+            <div className="About-info">
+                <p className="About-p">Diego Santos is a photographer and filmmaker based between León and his hometown in Asturias, in north Spain.</p>
+                <p className="About-p">His work resolves around his strong passion for mountains, sports, lifestyle and festivals.</p>
+            </div>
+            <div className="About-contact">
+                <p className="About-subp">Contact</p>
+                <a href="mailto:info@diegantos.com?subject=Hello%20there!" className="About-mail">info@diegantos.com</a>
+            </div>
         </div>
     )
 }
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,12 @@
 import { Footer } from "../Home/Footer/Footer"
 import { Header } from "../Home/Header/Header"
+import { About } from "./About/About"
 
 export const Contact = () => {
     return(
         <>
             <Header />
-            
+            <About />
             <Footer />
         </>
     )
