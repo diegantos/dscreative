8dd4c1ad8c8ac939598f4e965f457373900ddacb
Diegantos <diegantos8@gmail.com>
2023-02-02 17:55:19 +0100

    new up

--- src/components/Contact/About/About.jsx
+++ src/components/Contact/About/About.jsx
@@ -5,7 +5,7 @@ export const About = () => {
     return(
         <>
         <BackgroundElement 
-            url='/assets/PICOS/P1021096.jpg'
+            url='/assets/P1006460.jpg'
             clase='About-background' />
 
         <div className="About Wrapper">
@@ -13,9 +13,14 @@ export const About = () => {
                 <p className="About-p">Diego Santos is a photographer and filmmaker based between León and his hometown in Asturias, in north Spain.</p>
                 <p className="About-p">His work resolves around his strong passion for mountains, sports, lifestyle and festivals.</p>
             </div>
+
             <div className="About-contact">
                 <p className="About-subp">Contact</p>
                 <a href="mailto:info@diegantos.com?subject=Hello%20there!" className="About-mail">info@diegantos.com</a>
+            </div>
+
+            <div className="Grid">
+                
             </div>
         </div>
         </>
