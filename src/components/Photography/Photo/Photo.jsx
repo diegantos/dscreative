1d755d2a0f20ee906759a7787ec0f3d524c83726
Diegantos <diegantos8@gmail.com>
2023-02-02 19:49:37 +0100

    new grid

--- src/components/Photography/Photo/Photo.jsx
+++ src/components/Photography/Photo/Photo.jsx
@@ -3,26 +3,23 @@ import './Photo.scss';
 export const Photo = () => {
 
     const grid = [
-        { id : 0 , src : '' , alt : '' },
-        { id : 1 , src : '' , alt : '' },
-        { id : 2 , src : '' , alt : '' },
-        { id : 3 , src : '' , alt : '' },
-        { id : 4 , src : '' , alt : '' },
-        { id : 5 , src : '' , alt : '' },
-        { id : 6 , src : '' , alt : '' },
-        { id : 7 , src : '' , alt : '' },
-        { id : 8 , src : '' , alt : '' },
+        { id : 0 , src : '/assets/UBINAS/DJI_0005.jpg' , alt : 'Img' },
+        { id : 1 , src : '/assets/UBINAS/DJI_0011.jpg' , alt : 'Img' },
+        { id : 2 , src : '/assets/UBINAS/DJI_0021.jpg' , alt : 'Img' },
+        { id : 3 , src : '/assets/UBINAS/DJI_0025.jpg' , alt : 'Img' },
+        { id : 4 , src : '/assets/UBINAS/DJI_0027.jpg' , alt : 'Img' },
+        { id : 5 , src : '/assets/UBINAS/DJI_0031.jpg' , alt : 'Img' },
+        { id : 6 , src : '/assets/UBINAS/DJI_0049.jpg' , alt : 'Img' },
+        { id : 7 , src : '/assets/UBINAS/DJI_0056-2.jpg' , alt : 'Img' },
+        { id : 8 , src : '/assets/UBINAS/DJI_0077.jpg' , alt : 'Img' },
     ]
 
     return(
         <div className="Photo Wrapper">
             <div className="Photo-grid">
-                {grid.map()}
-                <img src="/assets/UBINAS/DJI_0005.jpg" alt="Img" />
-                <img src="/assets/UBINAS/DJI_0005.jpg" alt="Img" />
-                <img src="/assets/UBINAS/DJI_0005.jpg" alt="Img" />
-                <img src="/assets/UBINAS/DJI_0005.jpg" alt="Img" />
-                <img src="/assets/UBINAS/DJI_0005.jpg" alt="Img" />
+                {grid.map( ({ id , src , alt }) => 
+                    <img key={ id } src={ src } alt={ alt } loading="lazy" />
+                )}
             </div>
         </div>
     )
