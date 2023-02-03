61c50ce3df8422204a472980b71c8a36e3a16551
Diegantos <diegantos8@gmail.com>
2023-02-03 10:29:50 +0100

    new up

--- src/components/Photography/Photo/Photo.jsx
+++ src/components/Photography/Photo/Photo.jsx
@@ -3,22 +3,22 @@ import './Photo.scss';
 export const Photo = () => {
 
     const grid = [
-        { id : 0 , src : '/assets/UBINAS/DJI_0005.jpg' , alt : 'Img' },
-        { id : 1 , src : '/assets/UBINAS/DJI_0011.jpg' , alt : 'Img' },
-        { id : 2 , src : '/assets/UBINAS/DJI_0021.jpg' , alt : 'Img' },
-        { id : 3 , src : '/assets/UBINAS/DJI_0025.jpg' , alt : 'Img' },
-        { id : 4 , src : '/assets/UBINAS/DJI_0027.jpg' , alt : 'Img' },
-        { id : 5 , src : '/assets/UBINAS/DJI_0031.jpg' , alt : 'Img' },
-        { id : 6 , src : '/assets/UBINAS/DJI_0049.jpg' , alt : 'Img' },
-        { id : 7 , src : '/assets/UBINAS/DJI_0056-2.jpg' , alt : 'Img' },
-        { id : 8 , src : '/assets/UBINAS/DJI_0077.jpg' , alt : 'Img' },
+        { id : 0 , src : '/assets/UBINAS/DJI_0011.jpg' , alt : 'Img' , big : 'Big'},
+        { id : 1 , src : '/assets/UBINAS/DJI_0005.jpg' , alt : 'Img' , big : ''},
+        { id : 2 , src : '/assets/UBINAS/DJI_0021.jpg' , alt : 'Img' , big : ''},
+        { id : 3 , src : '/assets/UBINAS/DJI_0025.jpg' , alt : 'Img' , big : ''},
+        { id : 4 , src : '/assets/UBINAS/DJI_0027.jpg' , alt : 'Img' , big : ''},
+        { id : 5 , src : '/assets/UBINAS/DJI_0031.jpg' , alt : 'Img' , big : ''},
+        { id : 6 , src : '/assets/UBINAS/DJI_0049.jpg' , alt : 'Img' , big : ''},
+        { id : 7 , src : '/assets/UBINAS/DJI_0056-2.jpg' , alt : 'Img' , big : ''},
+        { id : 8 , src : '/assets/UBINAS/DJI_0077.jpg' , alt : 'Img' , big : ''},
     ]
 
     return(
         <div className="Photo">
             <div className="Photo-grid">
-                {grid.map( ({ id , src , alt }) => 
-                    <img key={ id } src={ src } alt={ alt } loading="lazy" />
+                {grid.map( ({ id , src , alt , big }) => 
+                    <img className={`Photo-img ${ big }`} key={ id } src={ src } alt={ alt } loading="lazy" />
                 )}
             </div>
         </div>
