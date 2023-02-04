2003cae6074e95c7264c241ffd08b7d6459a1280
Diegantos <diegantos8@gmail.com>
2023-02-04 11:19:46 +0100

    new grid distribution

--- src/components/Photography/Photo/Photo.jsx
+++ src/components/Photography/Photo/Photo.jsx
@@ -4,13 +4,13 @@ export const Photo = () => {
 
     const grid = [
         { id : 0 , src : '/assets/img/P1052054.jpg' , alt : 'Img' , big : 'Big'},
-        { id : 1 , src : '/assets/UBINAS/DJI_0005.jpg' , alt : 'Img' , big : ''},
+        { id : 1 , src : '/assets/img/P1085588.jpg' , alt : 'Img' , big : ''},
         { id : 2 , src : '/assets/UBINAS/DJI_0021.jpg' , alt : 'Img' , big : ''},
         { id : 3 , src : '/assets/UBINAS/DJI_0025.jpg' , alt : 'Img' , big : ''},
-        { id : 4 , src : '/assets/UBINAS/DJI_0027.jpg' , alt : 'Img' , big : ''},
-        { id : 5 , src : '/assets/UBINAS/DJI_0031.jpg' , alt : 'Img' , big : ''},
+        { id : 4 , src : '/assets/img/DJI_0027-Editar.jpg' , alt : 'Img' , big : ''},
+        { id : 5 , src : '/assets/UBINAS/DJI_0005.jpg' , alt : 'Img' , big : ''},
         { id : 6 , src : '/assets/UBINAS/DJI_0049.jpg' , alt : 'Img' , big : ''},
-        { id : 7 , src : '/assets/UBINAS/DJI_0056-2.jpg' , alt : 'Img' , big : ''},
+        { id : 7 , src : '/assets/UBINAS/DJI_0056-2.jpg' , alt : 'Img' , big : 'Big'},
         { id : 8 , src : '/assets/UBINAS/DJI_0077.jpg' , alt : 'Img' , big : ''},
     ]
 
@@ -22,6 +22,8 @@ export const Photo = () => {
                     <img className={`Photo-img ${ big }`} key={ id } src={ src } alt={ alt } loading="lazy" />
                 )}
             </div>
+
+            <h2 className="Photo-h2">Festival</h2>
         </div>
     )
 }
