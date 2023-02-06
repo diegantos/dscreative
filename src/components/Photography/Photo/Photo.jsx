ba8216c6b99028630755cb2bf39cd4c5da4de4a0
Diegantos <diegantos8@gmail.com>
2023-02-06 09:41:59 +0100

    new dist

--- src/components/Photography/Photo/Photo.jsx
+++ src/components/Photography/Photo/Photo.jsx
@@ -3,23 +3,26 @@ import './Photo.scss';
 export const Photo = () => {
 
     const grid = [
-        { id : 0 , src : '/assets/img/P1052054.jpg' , alt : 'Img' , big : 'Big'},
-        { id : 1 , src : '/assets/img/P1085588.jpg' , alt : 'Img' , big : ''},
-        { id : 2 , src : '/assets/UBINAS/DJI_0021.jpg' , alt : 'Img' , big : ''},
-        { id : 3 , src : '/assets/UBINAS/DJI_0025.jpg' , alt : 'Img' , big : ''},
-        { id : 4 , src : '/assets/img/DJI_0027-Editar.jpg' , alt : 'Img' , big : ''},
-        { id : 5 , src : '/assets/UBINAS/DJI_0005.jpg' , alt : 'Img' , big : ''},
-        { id : 6 , src : '/assets/UBINAS/DJI_0049.jpg' , alt : 'Img' , big : ''},
-        { id : 7 , src : '/assets/UBINAS/DJI_0056-2.jpg' , alt : 'Img' , big : 'Big'},
-        { id : 8 , src : '/assets/UBINAS/DJI_0077.jpg' , alt : 'Img' , big : ''},
+        { id : 0 , src : '/assets/img/P1052054.jpg' , alt : 'Img' , size : 'Big'},
+        { id : 1 , src : '/assets/img/P1085588.jpg' , alt : 'Img' , size : ''},
+        { id : 2 , src : '/assets/img/P1057378.jpg' , alt : 'Img' , size : ''},
+        { id : 3 , src : '/assets/img/P1021312-Editar.jpg' , alt : 'Img' , size : ''},
+        { id : 4 , src : '/assets/img/P1052087.jpg' , alt : 'Img' , size : ''},
+        { id : 5 , src : '/assets/img/P1069544-Editar.jpg' , alt : 'Img' , size : ''},
+        { id : 6 , src : '/assets/UBINAS/DJI_0049.jpg' , alt : 'Img' , size : ''},
+        { id : 7 , src : '/assets/img/P1085280.jpg' , alt : 'Img' , size : 'Big'},
+        { id : 8 , src : '/assets/img/P1054809.jpg' , alt : 'Img' , size : ''},
+        { id : 9 , src : '/assets/UBINAS/DJI_0077.jpg' , alt : 'Img' , size : ''},
+        { id : 10 , src : '/assets/img/DJI_0027-Editar.jpg' , alt : 'Img' , size : ''},
+        { id : 11 , src : '/assets/img/P1057962.jpg' , alt : 'Img' , size : ''},
     ]
 
     return(
         <div className="Photo">
             <h2 className="Photo-h2">Nature</h2>
             <div className="Photo-grid">
-                {grid.map( ({ id , src , alt , big }) => 
-                    <img className={`Photo-img ${ big }`} key={ id } src={ src } alt={ alt } loading="lazy" />
+                {grid.map( ({ id , src , alt , size }) => 
+                    <img className={`Photo-img ${ size }`} key={ id } src={ src } alt={ alt } loading="lazy" />
                 )}
             </div>
 
