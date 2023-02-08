1f26a5193f670cd76e90a19143909b67cf2d1abe
Diegantos <diegantos8@gmail.com>
2023-02-08 17:52:29 +0100

    new route path

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Contact } from './components/Contact/Contact'
 import { Home } from './components/Home/Home'
 import { Photography } from './components/Photography/Photography'
 import { Route, Routes } from 'react-router-dom'
+import { Videography } from './components/Videography/Videography'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/photography' element={<Photography />} />
+        <Route path='/video' element={<Videography />} />
       </Routes>
     
     </div>
---
+++ src/components/Videography/Videography.jsx
@@ -0,0 +1,13 @@
+import { Footer } from "../Home/Footer/Footer"
+import { Header } from "../Home/Header/Header"
+import { Video } from "./video/Video"
+
+export const Videography = () => {
+    return(
+        <>
+            <Header />
+            <Video />
+            <Footer />
+        </>
+    )
+}
