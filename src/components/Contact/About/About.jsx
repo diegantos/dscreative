f208aa45e9ad894feed8a1298688cf3649597e89
Diegantos <diegantos8@gmail.com>
2023-02-09 08:50:03 +0100

    Update About.jsx

--- src/components/Contact/About/About.jsx
+++ src/components/Contact/About/About.jsx
@@ -19,9 +19,6 @@ export const About = () => {
                 <a href="mailto:info@diegantos.com?subject=Hello%20there!" className="About-mail">info@diegantos.com</a>
             </div>
 
-            <div className="Grid">
-                
-            </div>
         </div>
         </>
     )
