ed1d6c2adc72f361dd82dd93c35eb772425d199a
Diego Santos <diegantos8@gmail.com>
2023-03-28 12:05:34 +0200

    Progress

--- src/components/Videography/video/Video.jsx
+++ src/components/Videography/video/Video.jsx
@@ -4,6 +4,7 @@ export const Video = () => {
     return(
         <div className="Video">
             <h2>WORK IN PROGRESS</h2>
+            <h4>CHECK OUT MY SOCIAL MEDIA FOR MORE</h4>
         </div>
     )
 }
