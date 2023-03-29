573de10d673d2c2597ae44a2d87b9a52a418e1a9
Diego Santos <diegantos8@gmail.com>
2023-03-29 09:33:03 +0200

    Trying scroll vid

--- src/components/Videography/video/Video.jsx
+++ src/components/Videography/video/Video.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from 'react'
+import { Btn } from '../../Common/Common'
 import './Video.scss'
 
 export const Video = () => {
+
+    const [show, setShow] = useState(false)
+    const [scrollTop, setScrollTop] = useState('')
+
+    const handleScroll = ( element ) => {
+        console.log(element)
+        let pixel = window.scrollY
+        console.log(pixel)
+        setScrollTop.currentTime = pixel / 1000
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    })
+
     return(
         <div className="Video">
-            <h2>WORK IN PROGRESS</h2>
-            <h4>CHECK OUT MY SOCIAL MEDIA FOR MORE</h4>
+            <div className="Video-vid">
+                <video className="Video-video" muted controls src="/assets/Videography.mp4"></video>
+                <Btn url='#' clase='Video-btn'>Click to continue</Btn>
+            </div>
         </div>
     )
 }
