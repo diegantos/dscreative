e673ad99272846707d0a1603fef60d4004c3bc20
Diego Santos <diegantos8@gmail.com>
2023-03-29 10:00:24 +0200

    Update Video.jsx

--- src/components/Videography/video/Video.jsx
+++ src/components/Videography/video/Video.jsx
@@ -1,30 +1,38 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Btn } from '../../Common/Common'
 import './Video.scss'
 
 export const Video = () => {
 
-    const [show, setShow] = useState(false)
+    const VideoPlayer = useRef(null)
     const [scrollTop, setScrollTop] = useState('')
 
-    const handleScroll = ( element ) => {
-        console.log(element)
-        let pixel = window.scrollY
-        console.log(pixel)
-        setScrollTop.currentTime = pixel / 1000
-    }
+    // const handleScroll = ( element ) => {
+    //     console.log(element)
+    //     let pixel = window.scrollY
+    //     console.log(pixel)
+    //     console.log(VideoPlayer)
+    //     VideoPlayer.currentTime = pixel / 1000
+    // }
 
     useEffect(() => {
-        window.addEventListener('scroll', handleScroll)
-        return () => {
-            window.removeEventListener('scroll', handleScroll)
-        }
+        window.addEventListener('scroll', () => {
+            let pixel = window.scrollY
+            VideoPlayer.currentTime = pixel / 1000
+            console.log(pixel)
+            console.log(VideoPlayer.currentTime)
+        })
+        // return () => {
+        //     window.removeEventListener('scroll', handleScroll)
+        // }
     })
 
     return(
         <div className="Video">
             <div className="Video-vid">
-                <video className="Video-video" muted controls src="/assets/Videography.mp4"></video>
+                <video
+                    ref={VideoPlayer} 
+                    className="Video-video" muted controls src="/assets/Videography.mp4"></video>
                 <Btn url='#' clase='Video-btn'>Click to continue</Btn>
             </div>
         </div>
